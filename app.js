dc1f8bd99d45d4a7adf13d2822e1d42011048d1f
theusgit <theusgit@outlook.com>
2021-02-05 20:39:35 -0300

    inserindo input no banco

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 'use strict';
 
-let banco=[
-    {'tarefa': 'Estudar Js', 'status': ''},
-    {'tarefa': 'Rezar', 'status': 'checked'},
+let banco = [
+    { 'tarefa': 'Estudar Js', 'status': '' },
+    { 'tarefa': 'Rezar', 'status': 'checked' },
 ]
 
-const criarItem = (text, status='') => {
+const criarItem = (text, status = '') => {
     const item = document.createElement('label');
     item.classList.add('todo_item');
     item.innerHTML = `
@@ -16,23 +16,70 @@ const criarItem = (text, status='') => {
     document.getElementById('todoList').appendChild(item);
 }
 
-const limparTarefas =()=>{
-    const todoList= document.getElementById('todoList');
-    while(todoList.firstChild){
+const limparTarefas = () => {
+    const todoList = document.getElementById('todoList');
+    while (todoList.firstChild) {
         todoList.removeChild(todoList.lastChild);
     }
 }
 
 
-const atualizarTela= ()=>{
+const atualizarTela = () => {
     limparTarefas();
-    banco.forEach(item=>criarItem(item.tarefa, item.status));
+    banco.forEach(item => criarItem(item.tarefa, item.status));
 }
 
+
+const inserirItem = (evento) => {
+    const tecla = evento.key;
+    const texto = evento.target.value;
+    if (tecla === 'Enter') {
+        banco.push({ 'tarefa': texto, 'status': '' });
+        atualizarTela();
+        evento.target.value = ''
+    }
+}
+
+
+
+document.getElementById('newItem').addEventListener('keypress', inserirItem);
+
+
+
+
+
+
+
+
+
+
+
+
 atualizarTela();
-atualizarTela();
-atualizarTela();
-atualizarTela();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
